3430be30c61a41f2726099971b41c3232ae76ed8
Chris Minnick <chris@minnick.com>
2022-10-05 06:24:46 -0700

    added dev tools

--- coder-fit-client/src/main.jsx
+++ coder-fit-client/src/main.jsx
@@ -7,6 +7,7 @@ import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 const client = new ApolloClient({
   uri: 'http://localhost:4000',
   cache: new InMemoryCache(),
+  connectToDevTools: true,
 });
 
 ReactDOM.createRoot(document.getElementById('root')).render(
